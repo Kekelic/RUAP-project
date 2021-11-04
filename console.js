d61c2bf3a37eeb8a3ea8f9a31580460436f36efc
Stjepan <kekelicstjepan@gmail.com>
2021-11-04 09:29:16 +0100

    added full message

--- console.js
+++ console.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 console.log("Pozdrav iz aplikacije od Stjepan Kekelić 000000000000")
+=======
+console.log("Pozdrav iz aplikacije od Stjepan Kekelić")
+>>>>>>> f12991f... added console.js file
